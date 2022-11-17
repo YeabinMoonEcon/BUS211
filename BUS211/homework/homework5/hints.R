54ff624d9e7f19ec00042a0bcc441c99e9614176
Yeabin Moon <32029208+YeabinMoonEcon@users.noreply.github.com>
2022-11-17 12:22:12 -0500

    6
    6

--- BUS211/homework/homework5/hints.R
+++ BUS211/homework/homework5/hints.R
@@ -5,10 +5,36 @@ setwd("~/Library/CloudStorage/Box-Box/data/SG/Patterns")
 patterns <- read_csv('your_data_sep_15_2022_0304pm.csv')
 
 
-temp <- place %>% select(placekey, naics_code)
+temp <- place %>% select(placekey, naics_code, top_category)
 
 df<-patterns %>% 
   left_join(temp, by ='placekey')
+
+cloth_df <-df %>% filter(top_category == 'Clothing Stores')
+
+cloth_df %>% count(location_name, sort = T) 
+
+cloth_df %>% filter(location_name == 'Old Navy') %>% 
+  select(placekey,location_name,street_address,date_range_start) %>%
+  arrange(street_address, date_range_start)
+
+
+cloth_df %>% count(placekey, sort= T) %>% select(n) %>% 
+  as.data.frame() %>% sapply(as.numeric) %>% 
+  hist(main = "Clothing")
+
+
+
+
+cloth_df %>% count(placekey, sort= T) %>% select(n) %>% as.data.frame(sapply(as.numeric)) %>% hist()
+b<-cloth_df %>% count(placekey, sort= T) %>% select(n) %>% as.data.frame()
+
+hist(b)
+a<-cloth_df %>% count(placekey, sort= T) %>% select(n) 
+hist(a)
+hist(a[[1]])
+
+names(cloth_df)
 
 glimpse(df)
 # find there are three data types
